a3ce5117145d98929b3899548bd18a05ac79b2e2
walyson <walysonmoises1342@gmail.com>
2023-07-15 23:32:08 -0300

    project created

---
+++ src/main/java/com/waly/walyLearn/WalyLearnApplication.java
@@ -0,0 +1,13 @@
+package com.waly.walyLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalyLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalyLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waly/walyLearn/WalyLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waly.walyLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalyLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
